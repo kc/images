a29928f577b8c3bd4466255d811eaf87806723f4
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-17 14:08:20 +0200

    Update vs2017.ps1

--- vs2017.ps1
+++ vs2017.ps1
@@ -15,6 +15,8 @@ $wu | Set-Service -StartupType Disabled
 choco install visualstudio2017-offline -ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --includeRecommended" -y
 choco install visualstudiocode -y
 
+choco install dotnetcore-sdk -y
+
 choco install vcredist2013 -y
 choco install sql-server-express -y
 choco install sql-server-management-studio -y
@@ -28,7 +30,7 @@ choco install git -y
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" -ErrorAction SilentlyContinue
-Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft VS Code\Code.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles)\Microsoft VS Code\Code.exe" -ErrorAction SilentlyContinue
 Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft SQL Server\130\Tools\Binn\ManagementStudio\Ssms.exe" -ErrorAction SilentlyContinue
 $ilspy = gci -Path "$env:ChocolateyInstall\lib\ILSpy*\tools\ILSpy.exe" | select -ExpandProperty FullName
 Install-ChocolateyPinnedTaskBarItem $ilspy -ErrorAction SilentlyContinue
