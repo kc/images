6edae906928ac8d7ccd4d7283dba37af52914305
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-17 13:34:27 +0200

    Update shutdown.ps1

--- shutdown.ps1
+++ shutdown.ps1
@@ -1,1 +1,1 @@
-Start-Process PowerShell -ArgumentList "-Command & { Start-Sleep -s 60; & shutdown -t 60 -s }"
+Start-Process PowerShell -ArgumentList "-Command & { Write-Host 'Waiting for boxstarter to finish and then shutdown.'; Start-Sleep -s 60; & shutdown -t 0 -s }"
