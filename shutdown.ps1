4ed6d0002499631b92cf0a9dcd83cdd09a2218a2
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-17 12:43:22 +0200

    Update shutdown.ps1

--- shutdown.ps1
+++ shutdown.ps1
@@ -1,7 +1,1 @@
-# Schedule automatic shutdown
-Write-Host "Creating a scheduled task for automatic shutdown."
-Write-Host "This task will run only once."
-
-$taskname = "boxstarter_ready"
-Get-ScheduledTask -TaskName $taskname -ea SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
-Register-ScheduledTask -TaskName $taskname -Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)) -Action (New-ScheduledTaskAction -Execute "shutdown" -Argument "/s /t 0")
+Start-Job -ScriptBlock { Start-Sleep -s 60; & shutdown -t 60 -s }
