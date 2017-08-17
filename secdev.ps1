3bd000b5359a0d3d46d2b8e1b597e175ae9ba220
Manuel Riezebosch <mriezebosch@gmail.com>
2017-08-17 15:21:19 +0200

    Update secdev.ps1

--- secdev.ps1
+++ secdev.ps1
@@ -12,8 +12,20 @@ choco install powershell -y --ignore-checksums
 $wu | Stop-Service
 $wu | Set-Service -StartupType Disabled
 
+#Enable Web Services
+choco install IIS-WebServerRole -source WindowsFeatures -y
+choco install IIS-ISAPIFilter -source WindowsFeatures -y
+choco install IIS-ISAPIExtensions -source WindowsFeatures -y
+
+#Enable ASP.NET on win 2012/8
+choco install IIS-NetFxExtensibility45 -source WindowsFeatures -y
+choco install NetFx4Extended-ASPNET45 -source WindowsFeatures -y
+choco install IIS-ASPNet45 -source WindowsFeatures -y
+choco install WCF-Services45 -source WindowsFeatures -y
+
 choco install visualstudio2017-offline -ia "--add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb" -y
 choco install visualstudiocode -y
+choco install dotnetcore-sdk -y
 
 choco install vcredist2013 -y
 choco install sql-server-express -y
@@ -25,7 +37,7 @@ choco install sumatrapdf.install -y --ignore-checksums
 
 Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe" -ErrorAction SilentlyContinue
 Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft VS Code\Code.exe" -ErrorAction SilentlyContinue
-Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft SQL Server\130\Tools\Binn\ManagementStudio\Ssms.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\Ssms.exe" -ErrorAction SilentlyContinue
 
 choco install winpcap
 choco install wireshark
@@ -39,7 +51,7 @@ choco install docker-toolbox -y
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 & docker-machine create --driver hyperv --hyperv-virtual-switch 'External Network' Default
-& "C:\Program Files\Docker Toolbox\docker-machine.exe" env | Invoke-Expression
+& docker-machine env | Invoke-Expression
 
 & docker run -p 8080:8080 -t -d --name webgoat webgoat/webgoat-7.1
 $ip = & docker-machine ip
