c6fb5b420bef9739a5fb5c65ae6e92f7ffef0840
Manuel Riezebosch <mriezebosch@gmail.com>
2017-09-25 16:58:05 +0200

    Update tfsadm.ps1

--- tfsadm.ps1
+++ tfsadm.ps1
@@ -1,2 +1,10 @@
-choco install sql-server -o -ia '/q /ACTION=Install /FEATURES=SQL,AS,RS,Conn,SDK,BC /INSTANCENAME=MSSQLSERVER  /SQLSVCINSTANTFILEINIT="True" /IACCEPTSQLSERVERLICENSETERMS'
-choco install sharepoint -y
+choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /SAPWD=P2ssw0rd'
+choco install sharepoint -ia '/config C:\VPC_IMAGES\config.xml'
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+$password = 'Pa$$w0rd' | ConvertTo-SecureString -AsPlainText -Force
+$cred =  New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $("administrator"), $password
+New-SPConfigurationDatabase -DatabaseName "SharePoint_Config" -DatabaseServer "." -FarmCredentials $cred -Passphrase $password
+
+& PSConfig.exe -cmd upgrade -inplace b2b -wait -cmd applicationcontent -install -cmd installfeatures -cmd secureresources -cmd services -install
