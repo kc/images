d92788bd733aff62d7a67f4c4820e699bf49a426
Manuel Riezebosch <mriezebosch@gmail.com>
2017-09-27 11:38:00 +0200

    Update tfsadm.ps1

--- tfsadm.ps1
+++ tfsadm.ps1
@@ -4,9 +4,16 @@ $wu | Set-Service -StartupType Manual | Stop-Service
 $env:sqlserver:isolocation = "C:\VPC_Images"
 choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /SAPWD=P2ssw0rd'
 
+$before = & choco list sharepoint -lo
+
 $wu | Start-Service
 choco install sharepoint -ia '/config C:\VPC_IMAGES\config.xml'
 $wu | Set-Service -StartupType Disabled
+
+$after = & choco list sharepoint -lo
+if ("$before" -ne "$after") {
+  Invoke-Reboot
+}
 
 Add-PSSnapin Microsoft.SharePoint.PowerShell
 
