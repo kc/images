d9eed96283a0443042d6bf1a02fca7ff60feec53
Manuel Riezebosch <mriezebosch@gmail.com>
2017-09-27 09:51:55 +0200

    Update tfsadm.ps1

--- tfsadm.ps1
+++ tfsadm.ps1
@@ -1,6 +1,12 @@
+$wu = Get-Service -Name wuauserv
+$wu | Set-Service -StartupType Manual | Stop-Service
+
 $env:sqlserver:isolocation = "C:\VPC_Images"
 choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /SAPWD=P2ssw0rd'
+
+$wu | Start-Service
 choco install sharepoint -ia '/config C:\VPC_IMAGES\config.xml'
+$wu | Set-Service -StartupType Disabled
 
 Add-PSSnapin Microsoft.SharePoint.PowerShell
 
