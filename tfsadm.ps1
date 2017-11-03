9e4abc4a39d9ab0bc122268a32155808427e80c0
gjtimmerman <31984609+gjtimmerman@users.noreply.github.com>
2017-11-03 14:20:12 +0100

    Removed Sharepoint installation

--- tfsadm.ps1
+++ tfsadm.ps1
@@ -3,22 +3,3 @@ $wu | Set-Service -StartupType Manual | Stop-Service
 
 $env:sqlserver:isolocation = "C:\VPC_Images"
 choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /SAPWD=P2ssw0rd'
-
-$before = & choco list sharepoint -lo
-
-$wu | Start-Service
-choco install sharepoint -ia '/config C:\VPC_IMAGES\config.xml'
-$wu | Set-Service -StartupType Disabled
-
-$after = & choco list sharepoint -lo
-if ("$before" -ne "$after") {
-  Invoke-Reboot
-}
-
-Add-PSSnapin Microsoft.SharePoint.PowerShell
-
-$password = 'Pa$$w0rd' | ConvertTo-SecureString -AsPlainText -Force
-$cred =  New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $("administrator"), $password
-New-SPConfigurationDatabase -DatabaseName "SharePoint_Config" -DatabaseServer $env:COMPUTERNAME -FarmCredentials $cred -Passphrase $password
-
-& PSConfig.exe -cmd upgrade -inplace b2b -wait -cmd applicationcontent -install -cmd installfeatures -cmd secureresources -cmd services -install
