e4b25d69e9cb558a35b3e4773a60b86ba6f230e3
gjtimmerman <31984609+gjtimmerman@users.noreply.github.com>
2017-11-10 09:20:44 +0100

    Update tfsadm.ps1
    Features op cmdline toegevoegd

--- tfsadm.ps1
+++ tfsadm.ps1
@@ -2,4 +2,4 @@ $wu = Get-Service -Name wuauserv
 $wu | Set-Service -StartupType Manual | Stop-Service
 
 $env:sqlserver:isolocation = "C:\VPC_Images"
-choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /SAPWD=P2ssw0rd'
+choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /FEATURES=SQL,AS,RS,Tools /SAPWD=P2ssw0rd'
