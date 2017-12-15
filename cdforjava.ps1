be2637ab2003176d6e29df10895f8571196b74bc
Johan Janssen <johanj@infosupport.com>
2017-12-15 11:22:24 +0100

    Created new configuration for the Continuous Delivery for Java training

---
+++ cdforjava.ps1
@@ -0,0 +1,16 @@
+choco install git -y
+setx PATH "$env:Path;$env:ProgramFiles\git\cmd"
+
+choco install jdk8 -y
+choco install intellijidea-ultimate -y 
+choco install eclipse -y 
+choco install maven -y
+choco install docker-for-windows -y --version 17.12.0.14746-edge
+choco install notepadplusplus -y
+choco install 7zip -y
+choco install sumatrapdf.install -y
+
+$jetbrain = gci -Path "$($Boxstarter.programFiles86)\JetBrains\IntelliJ*\bin\idea64.exe" | select -ExpandProperty FullName
+Install-ChocolateyPinnedTaskBarItem $jetbrain -ErrorAction SilentlyContinue
+
+Set-WindowsExplorerOptions -EnableShowFileExtensions
