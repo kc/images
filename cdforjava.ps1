3b94085081e14c525c52531aca9c06906d238740
johanjanssen <johanjanssen2001@gmail.com>
2018-03-16 12:28:42 +0100

    We copy and load the images instead of pulling to save network bandwith.

--- cdforjava.ps1
+++ cdforjava.ps1
@@ -15,15 +15,15 @@ choco install notepadplusplus -y
 choco install 7zip -y
 choco install sumatrapdf.install -y
 
-# Download Docker images
-docker pull jenkins/jenkins:2.89.2
-docker pull sameersbn/redis:latest
-docker pull sameersbn/postgresql:9.6-2
-docker pull sameersbn/gitlab:10.3.3
-docker pull cptactionhank/atlassian-jira-software:7.7.0
-docker pull sonarqube:6.7.1
-docker pull sonatype/nexus3:3.7.1
-docker pull registry:2
+# Load Docker images
+docker load -i C:\VPC_images\dockerimages\jenkins-2.89.2
+docker load -i C:\VPC_images\dockerimages\redis-latest
+docker load -i C:\VPC_images\dockerimages\postgresql-9.6-2
+docker load -i C:\VPC_images\dockerimages\gitlab-10.3.3
+docker load -i C:\VPC_images\dockerimages\atlassian-jira-software-7.7.0
+docker load -i C:\VPC_images\dockerimages\sonarqube-6.7.1
+docker load -i C:\VPC_images\dockerimages\nexus3-3.7.1
+docker load -i C:\VPC_images\dockerimages\registry-2
 
 $jetbrain = gci -Path "$($Boxstarter.programFiles86)\JetBrains\IntelliJ*\bin\idea64.exe" | select -ExpandProperty FullName
 Install-ChocolateyPinnedTaskBarItem $jetbrain -ErrorAction SilentlyContinue
