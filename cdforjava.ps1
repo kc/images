bc990f7f160f6127dcaf38331dd1ffdd44a8fb78
Johan Janssen <johanjanssen2001@gmail.com>
2018-12-05 08:58:34 +0100

    Update cdforjava.ps1
    Test Git issue

--- cdforjava.ps1
+++ cdforjava.ps1
@@ -1,29 +1,30 @@
-choco install git.install
+choco install git -y
 setx PATH "$env:Path;$env:ProgramFiles\git\cmd"
 
-choco install intellijidea-ultimate -y 
-choco install eclipse -y 
-choco install maven -y
-choco install docker-for-windows -y --version 18.06.1.19507
-choco install putty.install
+#choco install intellijidea-ultimate -y 
+#choco install eclipse -y 
+#choco install maven -y
+#choco install docker-for-windows -y --version 18.06.1.19507
+#choco install git.install
+#choco install putty.install
 
-choco install googlechrome -y
-choco install notepadplusplus -y
-choco install 7zip -y
-choco install sumatrapdf.install -y
-choco install nodejs.install -y
-choco install vscode -y
-choco install openjdk -y --version 11.0.1
+#choco install googlechrome -y
+#choco install notepadplusplus -y
+#choco install 7zip -y
+#choco install sumatrapdf.install -y
+#choco install nodejs.install -y
+#choco install vscode -y
+#choco install openjdk -y --version 11.0.1
 
 # Load Docker images
-docker load -i C:\VPC_images\dockerimages\jenkins.tar
-docker load -i C:\VPC_images\dockerimages\gitlab.tar
-docker load -i C:\VPC_images\dockerimages\jira.tar
-docker load -i C:\VPC_images\dockerimages\sonarqube.tar
-docker load -i C:\VPC_images\dockerimages\nexus.tar
-docker load -i C:\VPC_images\dockerimages\dockerregistry.tar
+#docker load -i C:\VPC_images\dockerimages\jenkins.tar
+#docker load -i C:\VPC_images\dockerimages\gitlab.tar
+#docker load -i C:\VPC_images\dockerimages\jira.tar
+#docker load -i C:\VPC_images\dockerimages\sonarqube.tar
+#docker load -i C:\VPC_images\dockerimages\nexus.tar
+#docker load -i C:\VPC_images\dockerimages\dockerregistry.tar
 
-$jetbrain = gci -Path "$($Boxstarter.programFiles86)\JetBrains\IntelliJ*\bin\idea64.exe" | select -ExpandProperty FullName
-Install-ChocolateyPinnedTaskBarItem $jetbrain -ErrorAction SilentlyContinue
+#$jetbrain = gci -Path "$($Boxstarter.programFiles86)\JetBrains\IntelliJ*\bin\idea64.exe" | select -ExpandProperty FullName
+#Install-ChocolateyPinnedTaskBarItem $jetbrain -ErrorAction SilentlyContinue
 
-Set-WindowsExplorerOptions -EnableShowFileExtensions
+#Set-WindowsExplorerOptions -EnableShowFileExtensions
