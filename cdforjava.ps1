850a43aaa7b1a4df45daf69ffe769608ac5963fd
Johan Janssen <johanjanssen2001@gmail.com>
2018-12-11 08:29:47 +0100

    Test for Git

--- cdforjava.ps1
+++ cdforjava.ps1
@@ -1,12 +1,12 @@
-choco install git -y
-setx PATH "$env:Path;$env:ProgramFiles\git\cmd"
+#choco install git -y
+#setx PATH "$env:Path;$env:ProgramFiles\git\cmd"
 
 choco install openjdk -y --version 11.0.1
 choco install intellijidea-ultimate -y 
 choco install eclipse -y --ignore-dependencies # Ignore JDK8 dependency
 choco install maven -y --ignore-dependencies # Ignore JDK8 dependency
 choco install docker-desktop -y --version 2.0.0.0
-choco install git.install
+#choco install git.install
 choco install putty.install
 
 choco install googlechrome -y
@@ -16,7 +16,7 @@ choco install sumatrapdf.install -y
 choco install nodejs.install -y
 choco install vscode -y
 
-choco uninstall git -y
+#choco uninstall git -y
 choco install git -y
 
 # Load Docker images
