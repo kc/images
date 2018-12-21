05629f87d64f59d9dfd6ee9f7ad06f69fad6b2d5
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2018-12-21 14:50:30 +0100

    Create GIT.ps1

---
+++ GIT.ps1
@@ -0,0 +1,34 @@
+#############################################################################
+# General options
+#############################################################################
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Disable-InternetExplorerESC
+C:\VPC_images\Functions\Enable-DoNotStartServerManagerAutomaticallyAtLogon.ps1
+
+#############################################################################
+# Remaining software installations
+#############################################################################
+
+choco install git -y # Includes git lfs & git credential manager for windows
+choco install kdiff3 -y
+choco install sourcetree -y
+choco install nodejs-lts -y # Used to serve git interactive cheat sheet (static website)
+choco install gource -y # Tool for visualizing git repo history
+choco install vscode -y
+choco install googlechrome -y
+choco install 7zip -y
+choco install sumatrapdf.install -y # 'sumatrapdf' only installs the cmdline tool, therefore use 'sumatrapdf.install'
+
+#############################################################################
+# Create task bar items
+#############################################################################
+
+$taskBarLinks = @(
+    ,"${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
+}
