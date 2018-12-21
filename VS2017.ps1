472a0be2ed3d8a8eaeead04312cf86fe746fc9e4
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2018-12-21 14:42:21 +0100

    Create VS2017.ps1

---
+++ VS2017.ps1
@@ -0,0 +1,66 @@
+#############################################################################
+# General options
+#############################################################################
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Disable-InternetExplorerESC
+C:\VPC_images\Functions\Enable-DoNotStartServerManagerAutomaticallyAtLogon.ps1
+
+#############################################################################
+# Windows Features
+#############################################################################
+
+Install-WindowsFeature Containers # For Windows Containers
+
+#############################################################################
+# Visual Studio 2017 Enterprise: Offline installation using layout directory.
+# 
+# Use below command to create a layout directory for offline installation. 
+# Make sure the same workloads you want to install are also in the layout.
+# The layout should be included in the cache.vhdx that is copied to the machine from TDS.
+# 
+# choco install visualstudio2017enterprise -y --params "--layout K:\visualstudio2017enterprise-layout --add workload1 --add workload2 --add component1 --includeRecommended"
+#############################################################################
+
+choco install visualstudio2017enterprise -y --execution-timeout=7000 `
+    --params "--bootstrapperPath K:\visualstudio2017enterprise-layout\vs_setup.exe" `
+    --ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.Net.Component.4.7.1.SDK --add Microsoft.Net.Component.4.7.1.TargetingPack --includeRecommended"
+
+#############################################################################
+# Remaining software installations
+#############################################################################
+
+choco install vscode -y --execution-timeout=7000
+choco install git -y # Includes git lfs & git credential manager for windows
+choco install nodejs-lts -y
+choco install docker-for-windows --version 18.06.1.19507 -y
+choco install docker-compose -y 
+choco install ILSpy -y
+choco install 7zip -y
+choco install googlechrome -y
+choco install sumatrapdf.install -y # 'sumatrapdf' only installs the cmdline tool, therefore use 'sumatrapdf.install'
+choco install firefox -y
+
+# We need this stuff for the DDAS training
+choco install sql-server-express --version 14.1801.3958.1 -y # SQL Server 2017 Express d.d. apr 2018
+choco install sql-server-management-studio --version 14.0.17285.0 -y # 17.9 d.d. sep 2018
+C:\VPC_images\Functions\Execute-SqlCmd.ps1 -sqlFile "C:\VPC_images\CustomScripts\CreateSchoolSampleDatabase.sql"
+
+#############################################################################
+# Fix Windows Search (Cortana)
+#############################################################################
+C:\VPC_images\Functions\Fix-WindowsSearch.ps1
+
+#############################################################################
+# Create task bar items
+#############################################################################
+
+$taskBarLinks = @(
+    ,"${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"
+    ,"${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
+}
