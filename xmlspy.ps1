793cc9600ce26cfd0427cad7261b049318aa7071
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2018-12-24 14:13:53 +0100

    Fix windows search, add shortcut to taskbar, set file explorer options

--- xmlspy.ps1
+++ xmlspy.ps1
@@ -1,3 +1,23 @@
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+
 choco install git
 choco install sumatrapdf.install
 choco install xmlspy
+
+#############################################################################
+# Fix Windows Search (Cortana)
+#############################################################################
+C:\VPC_images\Functions\Fix-WindowsSearch.ps1
+
+#############################################################################
+# Create task bar items
+#############################################################################
+
+$taskBarLinks = @(
+    ,"${env:ProgramFiles}\Altova\XMLSpy2018\XMLSpy.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
+}
