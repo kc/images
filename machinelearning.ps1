b032fd26854c1b0de1d3913d9dbf3ecae9e0c3b0
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2019-01-04 10:50:14 +0100

    Update machinelearning.ps1

--- machinelearning.ps1
+++ machinelearning.ps1
@@ -1,24 +1,29 @@
-$wu = Get-Service -Name wuauserv
-$wu | Set-Service -StartupType Manual | Stop-Service
+Set-WindowsExplorerOptions -EnableShowFileExtensions
 
 choco install googlechrome -y
 choco install 7zip -y
 choco install sumatrapdf.install -y
-
 choco install git -y
-setx PATH "$env:Path;$env:ProgramFiles\git\cmd"
-
 choco install R.Project -y
 choco install R.Studio -y
 choco install jdk8 -y
-choco install scala.install -y -ignoreDependencies
+#choco install scala.install -y -ignoreDependencies
 choco install imagemagick.app -y
-Install-ChocolateyZipPackage "spark-notebook" "https://s3.eu-central-1.amazonaws.com/spark-notebook/zip/spark-notebook-0.6.2-scala-2.10.4-spark-1.6.0-hadoop-2.2.0.zip?max-keys=100000" "C:\spark"
- 
-Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFiles)\RStudio\bin\rstudio.exe" -ErrorAction SilentlyContinue
+Install-ChocolateyZipPackage "spark-notebook" "https://s3.eu-central-1.amazonaws.com/spark-notebook/zip/spark-notebook-0.8.3-scala-2.11.8-spark-2.2.2-hadoop-2.7.2.zip?max-keys=100000" "C:\spark"
 
-Set-WindowsExplorerOptions -EnableShowFileExtensions
-if ($env:COMPUTERNAME -match "docent.") {
-    # Set the display to turn off after 1 hour
-    powercfg -x monitor-timeout-ac 60
+#############################################################################
+# Fix Windows Search (Cortana)
+#############################################################################
+C:\VPC_images\Functions\Fix-WindowsSearch.ps1
+
+#############################################################################
+# Create task bar items
+#############################################################################
+$taskBarLinks = @(
+    ,"${env:ProgramFiles}\RStudio\bin\rstudio.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
 }
