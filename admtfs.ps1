48b0bf151fe247a393d317f956127b4b199653e9
Joppe Catsburg <2551256+catsburg@users.noreply.github.com>
2019-01-04 10:04:23 +0100

    Delete admtfs.ps1

--- admtfs.ps1
+++
@@ -1,7 +0,0 @@
-$wu = Get-Service -Name wuauserv
-$wu | Set-Service -StartupType Manual | Stop-Service
-
-Enable-WindowsOptionalFeature -FeatureName Smtpsvc-Service-Update-Name -All -Online
-
-$env:sqlserver:isolocation = "C:\VPC_Images"
-choco install sql-server -ia '/CONFIGURATIONFILE=C:\VPC_IMAGES\ConfigurationFile.ini /FEATURES=SQL,AS,RS,Tools /SAPWD=P2ssw0rd'
