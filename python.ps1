019a12fffb48c0c78d832eb59713c9b455e83e73
douwevandenberg <48204124+douwevandenberg@users.noreply.github.com>
2019-03-22 12:52:58 +0100

    Updated python script
    Added cntk installation

--- python.ps1
+++ python.ps1
@@ -19,6 +19,9 @@ refreshenv
 # Revert to Python 3.6.8 due to bug in pycharm
 conda install python=3.6.8 -y
 
+# Install some packages
+pip install cntk
+
 #############################################################################
 # Fix Windows Search (Cortana)
 #############################################################################
