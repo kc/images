a326f94ae8c76db6f2d1d65da21c61d249135879
Johan Janssen <johanjanssen2001@gmail.com>
2019-08-26 15:23:04 +0200

    Pinned versions

--- cdforjava.ps1
+++ cdforjava.ps1
@@ -1,17 +1,17 @@
-choco install adoptopenjdk12 -y
-choco install intellijidea-ultimate -y 
-choco install eclipse -y
-choco install maven -y --ignore-dependencies # Ignore JDK8 dependency
+choco install adoptopenjdk12 -y --version 12.0.2.10
+choco install intellijidea-ultimate -y --version 2019.2
+choco install eclipse -y --version 4.12
+choco install maven -y --ignore-dependencies --version 3.6.1.20190711 # Ignore JDK8 dependency 
 choco install docker-desktop -y --version 2.0.0.3
-choco install putty.install
+choco install putty.install --version 0.72
 
-choco install googlechrome -y
-choco install notepadplusplus -y
-choco install 7zip -y
-choco install sumatrapdf.install -y
-choco install nodejs.install -y
-choco install vscode -y
-choco install git -y
+choco install googlechrome -y --version 76.0.3809.100
+choco install notepadplusplus -y --version 7.7.1
+choco install 7zip -y --version 19.0
+choco install sumatrapdf.install -y --version 3.1.2
+choco install nodejs.install -y --version 12.9.0
+choco install vscode -y --version 1.37.1
+choco install git -y --version 2.23.0
 
 # tool to show .md-slides
 npm i -g @infosupport/kc-cli
