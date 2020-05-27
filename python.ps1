b8ccf17e49975fb4d122afac13c1d4adaa4b3673
gjtimmerman <gertjan.timmerman@infosupport.com>
2020-05-27 08:59:08 +0200

    Added python, git, nodejs

--- python.ps1
+++ python.ps1
@@ -7,11 +7,17 @@ Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileEx
 # Software installations
 #############################################################################
 
+choco install python -y
+
 # Installing pycharm edu (18.3)
 choco install pycharm-edu -y
 
 # Installing Anaconda (python 3.x, v2018.12)
 choco install anaconda3 --params '"/AddToPath"' -y
+
+choco install git -y
+
+choco install nodejs -y
 
 choco install microsoft-teams -y
 
