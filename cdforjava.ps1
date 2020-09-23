0fd9208e8770bc792b305ef27ee453fa13072813
gjtimmerman <gertjan.timmerman@infosupport.com>
2020-09-23 09:20:52 +0200

    Upgraded eclipse to version 4.17

--- cdforjava.ps1
+++ cdforjava.ps1
@@ -1,6 +1,6 @@
 choco install adoptopenjdk12 -y --version 12.0.2.10
 choco install intellijidea-ultimate -y --version 2020.1
-choco install eclipse -y --version 4.14
+choco install eclipse -y --version 4.17
 choco install maven -y --ignore-dependencies --version 3.6.3   # Ignore JDK8 dependency 
 choco install docker-desktop -y --version 2.3.0.5
 choco install putty.install --version 0.73
