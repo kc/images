3d548d5e83a50bb13e7b20a0523e49feba20261d
jorisn <jorisn@infosupport.com>
2022-07-08 14:31:09 +0200

    Update javabase.ps1
    added docker-desktop to image

--- javabase.ps1
+++ javabase.ps1
@@ -8,6 +8,7 @@ choco install sourcetree -y
 choco install maven -y
 choco install wildfly -y 
 choco install openjdk17 -y
+choco install docker-desktop -y
 
 choco install vcredist2013 -y --version 12.0.40660.20180427
 choco install mysql -y 
