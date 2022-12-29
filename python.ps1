56786fbe9330e4a57f36f27d58ec7aed4ca8a2fc
gjtimmerman <gertjan.timmerman@infosupport.com>
2022-12-29 07:54:16 +0100

    Removed -NoExit

--- python.ps1
+++ python.ps1
@@ -25,7 +25,7 @@ choco install microsoft-teams -y
 # Refresh path variables
 refreshenv
 
-c:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\tools\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\tools\Anaconda3' ; conda env create -f C:\VPC_Images\pythonai.yaml"
+c:\windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -Command "& 'C:\tools\Anaconda3\shell\condabin\conda-hook.ps1' ; conda env create -f C:\VPC_Images\pythonai.yaml"
 
 
 #############################################################################
