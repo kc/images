65e112e00f0d7a3dde05c2814518455f92b8318f
gjtimmerman <gertjan.timmerman@infosupport.com>
2024-07-16 15:09:59 +0200

    Updating Microsoft Teams

--- git.ps1
+++ git.ps1
@@ -19,7 +19,11 @@ choco install vscode -y
 choco install googlechrome -y
 choco install 7zip -y
 choco install sumatrapdf.install -y # 'sumatrapdf' only installs the cmdline tool, therefore use 'sumatrapdf.install'
-choco install microsoft-teams -y
+choco install webview2-runtime -y
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowAllTrustedApps" /d "1"
+
+Dism /Online /Add-ProvisionedAppxPackage /PackagePath:"K:\microsoft-teams-new\MSTeams-x64.msix" /SkipLicense
 
 #############################################################################
 # Create task bar items
