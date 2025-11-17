89f363e0ca1ac069fa907e1c5e4aa3e13c8efdd1
Gert Jan Timmerman <gertt@infosupport.com>
2025-11-17 19:27:10 +0100

    Added vs2026 files

---
+++ vs2026.ps1
@@ -0,0 +1,89 @@
+#############################################################################
+# General options
+#############################################################################
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Disable-InternetExplorerESC
+C:\VPC_images\Functions\Enable-DoNotStartServerManagerAutomaticallyAtLogon.ps1
+
+#############################################################################
+# Windows Features
+#############################################################################
+
+Install-WindowsFeature Containers # For Windows Containers
+
+#############################################################################
+# Visual Studio 2022 Enterprise: Offline installation using layout directory.
+# 
+# Use below command to create a layout directory for offline installation. 
+# The layout should be included in the cache.vhdx that is copied to the machine from TDS.
+#
+# IMPORTANT: Delete the entire layout directory (K:\visualstudio2022enterprise-layout) before creating a new one.
+# IMPORTANT: Make sure the correct version and included workloads is used when creating the layout.
+# 
+# choco install visualstudio2022enterprise --version 117.14.16.0 -y --params "--layout K:\visualstudio2022enterprise-layout --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.Data --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --add Microsoft.VisualStudio.ComponentGroup.ArchitectureTools.Managed --add Microsoft.VisualStudio.Component.AzureDevOps.OfficeIntegration --add Microsoft.VisualStudio.Component.Git --add Microsoft.VisualStudio.Component.DependencyValidation.Enterprise --includeRecommended" --force
+#############################################################################
+
+choco install visualstudio2022enterprise --version 117.14.16.0 -y --execution-timeout=7000 `
+    --params "--bootstrapperPath K:\visualstudio2022enterprise-layout\vs_setup.exe" `
+    --ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Workload.Data --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --add Microsoft.VisualStudio.ComponentGroup.ArchitectureTools.Managed --add Microsoft.VisualStudio.Component.AzureDevOps.OfficeIntegration --add Microsoft.VisualStudio.Component.Git --add Microsoft.VisualStudio.Component.DependencyValidation.Enterprise --includeRecommended"
+
+#############################################################################
+# Remaining software installations
+#############################################################################
+
+choco install dotnet-sdk -y
+choco install vscode -y --execution-timeout=7000
+choco install git -y # Includes git lfs & git credential manager for windows
+choco install azure-cli -y
+choco install azure-functions-core-tools --params "/x64:true" -y
+choco install az.powershell -y
+choco install azurefunctions-vscode -y
+choco install vscode-csharp -y
+choco install vscode-powershell -y
+choco install nodejs-lts -y
+choco install docker-desktop --version 4.16.0 -y
+choco install docker-compose -y 
+choco install ILSpy -y
+choco install 7zip -y
+choco install googlechrome -y
+choco install sumatrapdf.install -y # 'sumatrapdf' only installs the cmdline tool, therefore use 'sumatrapdf.install'
+choco install firefox -y
+choco install webview2-runtime -y
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowAllTrustedApps" /d "1"
+
+Dism /Online /Add-ProvisionedAppxPackage /PackagePath:"K:\microsoft-teams-new\MSTeams-x64.msix" /SkipLicense
+
+
+
+
+# We need this stuff for the DDAS training
+choco install sql-server-express-adv --version 16.0.1000.6 -y # SQL Server 2022 Express Adv d.d. feb 2023
+choco install sql-server-management-studio --version 21.4.8 -y # 21.4 d.d. juli 2025
+C:\VPC_images\Functions\Execute-SqlCmd.ps1 -sqlFile "C:\VPC_images\CustomScripts\CreateSchoolSampleDatabase.sql"
+C:\VPC_images\Functions\Execute-SqlCmd.ps1 -sqlFile "C:\VPC_images\CustomScripts\adventureworkslt.sql"
+
+# Remove-Item $env:APPDATA\NuGet\nuget.config
+
+dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
+
+#############################################################################
+# Fix Windows Search (Cortana)
+#############################################################################
+C:\VPC_images\Functions\Fix-WindowsSearch.ps1
+
+#############################################################################
+# Create task bar items
+#############################################################################
+
+$taskBarLinks = @(
+    ,"${env:ProgramFiles}\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe"
+    ,"${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
+}
+
---
+++ vs2026Only.ps1
@@ -0,0 +1,78 @@
+#############################################################################
+# General options
+#############################################################################
+
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Disable-InternetExplorerESC
+C:\VPC_images\Functions\Enable-DoNotStartServerManagerAutomaticallyAtLogon.ps1
+
+#############################################################################
+# Windows Features
+#############################################################################
+
+Install-WindowsFeature Containers # For Windows Containers
+
+#############################################################################
+# Visual Studio 2022 Enterprise: Offline installation using layout directory.
+# 
+# Use below command to create a layout directory for offline installation. 
+# The layout should be included in the cache.vhdx that is copied to the machine from TDS.
+#
+# IMPORTANT: Delete the entire layout directory (K:\visualstudio2022enterprise-layout) before creating a new one.
+# IMPORTANT: Make sure the correct version and included workloads is used when creating the layout.
+# 
+# choco install visualstudio2022enterprise --version 117.14.16.0 -y --params "--layout K:\visualstudio2022enterprise-layout --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended" --force
+#############################################################################
+
+choco install visualstudio2022enterprise --version 117.14.16.0 -y --execution-timeout=7000 `
+    --params "--bootstrapperPath K:\visualstudio2022enterprise-layout\vs_setup.exe" `
+    --ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended"
+
+#############################################################################
+# Remaining software installations
+#############################################################################
+
+choco install dotnet-sdk -y
+choco install vscode -y --execution-timeout=7000
+choco install git -y # Includes git lfs & git credential manager for windows
+choco install azure-cli -y
+choco install azure-functions-core-tools --params "/x64:true" -y
+choco install az.powershell -y
+choco install azurefunctions-vscode -y
+choco install vscode-csharp -y
+choco install vscode-powershell -y
+choco install nodejs-lts -y
+choco install ILSpy -y
+choco install 7zip -y
+choco install googlechrome -y
+choco install sumatrapdf.install -y # 'sumatrapdf' only installs the cmdline tool, therefore use 'sumatrapdf.install'
+choco install firefox -y
+choco install webview2-runtime -y
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowAllTrustedApps" /d "1"
+
+Dism /Online /Add-ProvisionedAppxPackage /PackagePath:"K:\microsoft-teams-new\MSTeams-x64.msix" /SkipLicense
+
+
+# Remove-Item $env:APPDATA\NuGet\nuget.config
+
+dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
+
+#############################################################################
+# Fix Windows Search (Cortana)
+#############################################################################
+C:\VPC_images\Functions\Fix-WindowsSearch.ps1
+
+#############################################################################
+# Create task bar items
+#############################################################################
+
+$taskBarLinks = @(
+    ,"${env:ProgramFiles}\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe"
+    ,"${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+)
+
+foreach($taskBarLink in $taskBarLinks)
+{
+    C:\VPC_images\Tools\syspin.exe $taskBarLink c:5386
+}
