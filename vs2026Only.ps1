2067918c7f02ea9abfdf372effa7f04c3f1f3ffe
Gert Jan Timmerman <gertt@infosupport.com>
2025-11-18 10:40:00 +0100

    Edited vs2026* files

--- vs2026Only.ps1
+++ vs2026Only.ps1
@@ -13,20 +13,20 @@ C:\VPC_images\Functions\Enable-DoNotStartServerManagerAutomaticallyAtLogon.ps1
 Install-WindowsFeature Containers # For Windows Containers
 
 #############################################################################
-# Visual Studio 2022 Enterprise: Offline installation using layout directory.
+# Visual Studio 2026 Enterprise: Offline installation using layout directory.
 # 
 # Use below command to create a layout directory for offline installation. 
 # The layout should be included in the cache.vhdx that is copied to the machine from TDS.
 #
-# IMPORTANT: Delete the entire layout directory (K:\visualstudio2022enterprise-layout) before creating a new one.
+# IMPORTANT: Delete the entire layout directory (K:\visualstudio2026enterprise-layout) before creating a new one.
 # IMPORTANT: Make sure the correct version and included workloads is used when creating the layout.
 # 
-# choco install visualstudio2022enterprise --version 117.14.16.0 -y --params "--layout K:\visualstudio2022enterprise-layout --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended" --force
+# choco install visualstudio2026enterprise --version 118.0.0.0 -y --params "--layout K:\visualstudio2026enterprise-layout --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop  --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.NetCore.Component.Runtime.10.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended" --force
 #############################################################################
 
-choco install visualstudio2022enterprise --version 117.14.16.0 -y --execution-timeout=7000 `
-    --params "--bootstrapperPath K:\visualstudio2022enterprise-layout\vs_setup.exe" `
-    --ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.NetCore.Component.Runtime.6.0 --add Microsoft.NetCore.Component.Runtime.7.0 --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended"
+choco install visualstudio2026enterprise --version 118.0.0.0 -y --execution-timeout=7000 `
+    --params "--bootstrapperPath K:\visualstudio2026enterprise-layout\vs_setup.exe" `
+    --ia "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.NetCore.Component.Runtime.8.0 --add Microsoft.NetCore.Component.Runtime.9.0 --add Microsoft.NetCore.Component.Runtime.10.0 --add Microsoft.Net.Component.4.8.1.SDK --add Microsoft.Net.Component.4.8.1.TargetingPack --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup --includeRecommended"
 
 #############################################################################
 # Remaining software installations
